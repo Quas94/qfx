56394704682b982a4aa5e829df2b453e9d203fa7
Michael <quasar.exe@hotmail.com>
2016-03-11 15:33:57 +1100

    Add libjson and Poco libraries

--- src/qfx.cpp
+++ src/qfx.cpp
@@ -4,35 +4,88 @@
  * Author: Michael
  */
 
-#include <cstdio>
 #include <cstdlib>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <sstream>
 
 #include "ta_libc.h"
+#include "libjson.h"
 
-int main(int argc, char** argv) {
-	printf("Starting...\n");
+#include <Poco/Net/HTTPClientSession.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/StreamCopier.h>
+#include <Poco/Path.h>
+#include <Poco/URI.h>
+#include <Poco/Exception.h>
 
-	double inputs[50];
-	for (int i = 0; i < 50; i++) {
-		inputs[i] = i;
-	}
+using namespace Poco;
+using namespace Poco::Net;
+using namespace std;
 
-	int outStart;
-	int outEnd;
+// Edit these global variables with your own information
 
-	TA_RetCode code = TA_MA(0, 50, &inputs[0], TA_INTEGER_DEFAULT, TA_MAType_SMA, &outStart, &outEnd, &inputs[0]);
+std::string account_id = "1234567";
+std::string access_token = "ACCESS-TOKEN";
+std::string instruments = "EUR_USD,USD_CAD,EUR_JPY";
+std::string domain = "https://stream-fxpractice.oanda.com";
 
-	printf("outStart = %d, outEnd = %d\n", outStart, outEnd);
+/*****************************
+The domain variable should be:
+For Sandbox    -> https://stream-sandbox.oanda.com
+For fxPractice -> https://stream-fxpractice.oanda.com
+For fxTrade    -> https://stream-fxtrade.oanda.com
+******************************/
 
-	for (int i = 0; i < 50; i++) {
-		if (i > 0) {
-			printf(" ");
+void handleStream(streambuf* stream_buffer)
+{
+	std::istreambuf_iterator<char> eos; // end-of-range iterator
+	std::istreambuf_iterator<char> iit(stream_buffer); // stream iterator
+	string str;
+
+	while (iit != eos) {
+		ostringstream oss;
+		while (*iit != '\n') {
+			oss << *iit++;
 		}
-		printf("%lf", inputs[i]);
-		if (i < 49) {
-			printf(",");
-		}
-	}
 
-	return EXIT_SUCCESS;
+		//print the tick 
+		cout << oss.str() << endl;
+
+		*iit++;
+	}
+}
+
+int main()
+{
+	try {
+		// prepare session
+		URI uri(domain + std::string("/v1/prices?accountId=") + account_id + std::string("&instruments=") + instruments);
+
+		HTTPClientSession session(uri.getHost(), uri.getPort());
+		session.setKeepAlive(true);
+
+		// prepare path
+		string path(uri.getPathAndQuery());
+		if (path.empty()) path = "/";
+
+		// send request
+		HTTPRequest req(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
+		req.set("Authorization", std::string("Bearer ") + access_token);
+		session.sendRequest(req);
+
+		// get response
+		HTTPResponse res;
+		istream& rs = session.receiveResponse(res);
+
+		// handle response
+		ostringstream out_string_stream;
+		handleStream(rs.rdbuf());
+	}
+	catch (const Exception &e)
+	{
+		cerr << e.displayText() << endl;
+	}
 }
